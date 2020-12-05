7c12aad6355c68bf0110726f2053a88c42774901
RusinTomasz <tomaszrusin90@gmail.com>
2020-12-05 10:39:55 +0100

    colorbox

--- web/themes/custom/rmeble/js/cs-colorobox.js
+++ web/themes/custom/rmeble/js/cs-colorobox.js
@@ -18,26 +18,57 @@
       };
 
       $(".colorbox").colorbox(cboxOptions);
-     
 
       var resizeTimer;
       function resizeColorBox() {
         if (resizeTimer) clearTimeout(resizeTimer);
         resizeTimer = setTimeout(function () {
           if (jQuery("#cboxOverlay").is(":visible")) {
-            jQuery.colorbox.resize({ width: "95%", maxHeight: "95%" });
+            jQuery.colorbox.resize({
+              width: "95%",
+              maxHeight: "95%",
+            });
           }
         }, 300);
       }
 
+      // window.addEventListener(
+      //   "orientationchange",
+      //   function () {
+      //     if ($("#cboxOverlay").is(":visible")) {
+      //       resizeColorBox();
+      //     }
+      //   },
+      //   false
+      // );
+
+      // var colorboxResize = function (resize) {
+      //   var width = "95%";
+      //   var height = "95%";
+
+      //   $.colorbox.settings.height = height;
+      //   $.colorbox.settings.width = width;
+
+      //   //if window is resized while lightbox open
+      //   if (resize) {
+      //     $.colorbox.resize({
+      //       height: height,
+      //       width: width,
+      //     });
+      //   }
+      // };
+
       // Resize Colorbox when resizing window or changing mobile device orientation
+
       jQuery(window).resize(resizeColorBox);
+
       window.addEventListener("orientationchange", resizeColorBox, false);
       var settings = {
         colorbox: {
           mobiledetect: true,
         },
       };
+
       if (settings.colorbox.mobiledetect && window.matchMedia) {
         // Disable Colorbox for small screens.
         var mq = window.matchMedia(
@@ -50,6 +81,7 @@
       settings.colorbox.rel = function () {
         return $(this).data("colorbox-gallery");
       };
+
       $(".colorbox", context).once("init-colorbox").colorbox(settings.colorbox);
     },
   };
